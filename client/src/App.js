cd24a4c5de60e10e97ef701162db59bf93d88ee6
Adam Carter <mail@adam-carter.com>
2018-10-08 17:40:13 -0700

    started playing around with calendar component.

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+class Calendar extends Component{
+   constructor(props){
+      super(props);
+
+      let today = new Date();
+      let year_range = [];
+      for(let  i = today.getFullYear() - 3; i < today.getFullYear() + 5; i++){
+         year_range.push(i);
+      }
+
+      this.state = {
+         years: year_range,
+         selectedMonth: today.getMonth(),
+         selectedYear: today.getYear(),
+      };
+   }
+
+   render(){
+      return(
+      <div>
+         <select>
+            {MONTHS.map( (month) =>
+               <option value="{index}">{month}</option>
+            )}
+            
+         </select>
+      </div>
+      );
+   }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Calendar />
       </div>
     );
   }
